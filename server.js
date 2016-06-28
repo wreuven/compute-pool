f875bc25f6a107bd79f8f5907b8acc4848ef36e9
wreuven <wreuven@users.noreply.github.com>
2016-06-28 16:38:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port       = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});
+var http = require('http');
+ 
+http.createServer(  function (request, response){
+ 
+ response.writeHead(200);
+ 
+ request.on('data', function(message) {
+ 
+   response.write(message);
+ 
+ });
+ 
+ request.on('end', function() {
+    response.end();
+ });
+ 
+}).listen(server_port);
